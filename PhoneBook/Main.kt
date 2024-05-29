8c2b7138db9d490431c6b551cf604534d3961545
Serega1067 <serega1067@yandex.ru>
2024-05-29 19:08:42 +0700

    Modified file Main

--- PhoneBook/Main.kt
+++ PhoneBook/Main.kt
@@ -1,11 +1,39 @@
-fun receivingAndVerifyingData(): List<String> {
+fun inputData(): List<String> {
     val arrayCommands = readln().split(' ')
     return arrayCommands
 }
 
+fun checkingCommand(command: List<String>): List<String> {
+    if (command[0] == "add") {
+        checkingAddCommand(command)
+    }
+}
+
+fun checkingAddCommand(command: List<String>) {
+    val result = Array(4, {"null"})
+    result[0] = "add"
+    if (command[1] == "phone") {
+        checkingPhone()
+    }
+}
+
+fun executingCommands(command: List<String>): List<String> {
+    when (command[0]) {
+        "exit" -> println("The program is over")
+        "add" -> println("${command[1]} ${command[2]} ${command[3]}")
+        "help" -> println("""
+                             add - adding data
+                             help - help
+                             exit - exit the program
+                          """.trimIndent())
+        else -> println("Error")
+    }
+    return command
+}
+
 fun main() {
     do {
-        val arrayCommands = receivingAndVerifyingData()
+        val arrayCommands = executingCommands(inputData())
         println(arrayCommands.joinToString())
         println(arrayCommands::class.simpleName)
     } while (arrayCommands[0] != "exit")
